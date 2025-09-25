78eaefa91e637a5c7e0b5a9f8ea4e72e1b561288
Roshan <roshanochu09@gmail.com>
2025-09-25 12:38:55 +0530

    firebase setup done

---
+++ lib/presentation/auth/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
